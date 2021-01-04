e309462ef34912cb408034749ac9947c507168c0
Arkater <valentin.td.monteiro@gmail.com>
2021-01-04 12:39:37 +0100

    Add selection artiste

---
+++ Art_fundingV0/Controllers/SelectionArtisteController.cs
@@ -0,0 +1,30 @@
+﻿using Art_fundingV0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Art_fundingV0.Controllers
+{
+    public class SelectionArtisteController : Controller
+    {
+        private IDalArtiste dalArtiste;
+        public SelectionArtisteController() : this(new DalArtiste())
+        {
+        }
+        private SelectionArtisteController(IDalArtiste dalIoc)
+        {
+            dalArtiste = dalIoc;
+        }
+        // GET: SelectionArtiste
+        public ActionResult SelectionArtiste()
+        {
+            List<artiste> listArtiste = new List<artiste>();
+            listArtiste = dalArtiste.ObtientTousLesArtistes();
+
+            return View(listArtiste);
+        }
+
+    }
+}
